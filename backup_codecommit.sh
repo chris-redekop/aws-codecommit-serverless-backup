582872a213b935345a944f6cfd49cc1437414786
Chris Redekop <chris.redekop@gmail.com>
2019-01-26 06:11:20 -0500

    Remove timestamps from filenames

--- backup_codecommit.sh
+++ backup_codecommit.sh
@@ -28,12 +28,11 @@ do
     echo "[===== Cloning repository: ${codecommitrepo} =====]"
     git clone "https://git-codecommit.${AWS_DEFAULT_REGION}.amazonaws.com/v1/repos/${codecommitrepo}"
 
-    dt=$(date -u '+%Y_%m_%d_%H_%M')
-    zipfile="${codecommitrepo}_backup_${dt}_UTC.tar.gz"
+    zipfile="${codecommitrepo}.tar.gz"
     echo "Compressing repository: ${codecommitrepo} into file: ${zipfile} and uploading to S3 bucket: ${backup_s3_bucket}/${codecommitrepo}"
 
     tar -zcvf "${zipfile}" "${codecommitrepo}/"
-    aws s3 cp "${zipfile}" "s3://${backup_s3_bucket}/${codecommitrepo}/${zipfile}" --region $AWS_DEFAULT_REGION
+    aws s3 cp "${zipfile}" "s3://${backup_s3_bucket}/${zipfile}" --region $AWS_DEFAULT_REGION
 
     rm $zipfile
     rm -rf "$codecommitrepo"
